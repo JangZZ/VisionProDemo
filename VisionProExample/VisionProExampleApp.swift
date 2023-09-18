a4ace626add7da182f661fa7086f33b14c9f40b2
Andy <giangnt078@gmail.com>
2023-09-18 21:48:11 +0700

    [DEV] - initial project

---
+++ VisionProExample/VisionProExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionProExampleApp.swift
+//  VisionProExample
+//
+//  Created by Andy on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct VisionProExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
