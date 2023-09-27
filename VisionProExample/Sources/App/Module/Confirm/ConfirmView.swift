c9ace7f48c6c21608819e24d0074fdc040cd2b99
Andy <giangnt078@gmail.com>
2023-09-27 11:12:35 +0700

    [DEV] - update UI

--- VisionProExample/Sources/App/Module/Confirm/ConfirmView.swift
+++ VisionProExample/Sources/App/Module/Confirm/ConfirmView.swift
@@ -13,19 +13,25 @@ struct ConfirmView: View {
     @EnvironmentObject var navigator: AppNavigator
 
     var body: some View {
-        VStack(alignment: .center, spacing: 8) {
+        VStack(spacing: 8) {
+            Image(.icLogoRed)
+                .resizable()
+                .frame(width: 250, height: 40, alignment: .leading)
+                .frame(maxWidth: .infinity,  alignment: .leading)
+            
             Text("Transfer VND \(amount)")
                 .font(.extraLargeTitle2)
                 .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.bottom, 24)
+                .padding(.vertical, 24)
+            
             infoView(title: "To",
                      name: "BUI MINH TU",
                      content: "1900 2222 3333 99\nJoint Stock Commercial Bank for Foreign Trade of Vietnam")
             .padding(.bottom, 24)
             infoView(title: "From",
                      name: "Current Account",
-                     content: "1903 2131 3232 22")
-            HStack {
+                     content: "888888888")
+            VStack(alignment: .leading) {
                 HStack {
                     Text("Message: ")
                         .font(.system(size: 25, weight: .bold))
@@ -33,16 +39,20 @@ struct ConfirmView: View {
                         .foregroundStyle(.blue)
                         .font(.system(size: 22, weight: .bold))
                 }
-                Spacer()
+                
                 HStack {
                     Text("Transfer fee: ")
                         .font(.system(size: 25, weight: .bold))
+                        .multilineTextAlignment(.leading)
+                    
                     Text("VND 0")
                         .font(.system(size: 22, weight: .bold))
                         .foregroundStyle(.blue)
                 }
             }
             .padding(.top, 16)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
             Button(action: {
                 navigator.present(sheet: .result(message, amount))
             }) {
@@ -51,7 +61,7 @@ struct ConfirmView: View {
             }
             .padding(.top, 24)
         }
-        .padding(.all, 16)
+        .padding(.all, 30)
     }
     
     @ViewBuilder func infoView(title: String = "",
@@ -61,6 +71,7 @@ struct ConfirmView: View {
             Text(title)
                 .font(.title)
                 .frame(maxWidth: .infinity, alignment: .leading)
+            
             HStack(alignment: .top, spacing: 16) {
                 Image(.icLogo)
                     .iconified(imageSize: 40, surroundSize: 60, color: .white)
